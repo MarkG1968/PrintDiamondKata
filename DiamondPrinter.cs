c025cb07bb3d2382774dc24605c07746d9a68976
MarkG1968 <mark.gravestock@gmail.com>
2015-01-10 19:39:47 +0000

    Add first 3 tests

---
+++ DiamondPrinter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkG1968.PrintDiamondKata
+{
+    class DiamondPrinter
+    {
+        internal String PrintDiamondFor(char character)
+        {
+            if (character == 'A')
+            {
+                return "A";
+            }
+
+            return  " A\n" +
+                    "B B\n" +
+                    " A";
+
+        }
+    }
+}
---
+++ PrintDiamondTests.cs
@@ -0,0 +1,35 @@
+﻿using Should.Fluent;
+
+namespace MarkG1968.PrintDiamondKata
+{
+    public class PrintDiamondTests
+    {
+        private DiamondPrinter sut;
+
+        public PrintDiamondTests()
+        {
+            sut = new DiamondPrinter();
+        }
+
+        public void PrintsADiamondForA()
+        {
+            sut.PrintDiamondFor('A').Should().Equal("A");
+        }
+ 
+        public void PrintsADiamondForB()
+        {
+            sut.PrintDiamondFor('B').Should().Equal(" A\nB B\n A");
+        }
+
+        public void PrintsADiamondForC()
+        {
+            var expected = "  A\n" +
+                           " B B\n" +
+                           "C   C\n" +
+                           " B B\n" +
+                           "  A";
+
+            sut.PrintDiamondFor('C').Should().Equal(expected);
+        }
+    }
+}
